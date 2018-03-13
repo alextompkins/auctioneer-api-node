c8000997ec644cc1563d3b7a2aed1748995c09dc
alextompkins <ato47@uclive.ac.nz>
2018-03-14 09:31:09 +1300

    Removing unneccesary getConnection() promise

--- app/models/users.server.model.js
+++ app/models/users.server.model.js
@@ -4,10 +4,7 @@ exports.create = function(values, done) {
     const createSQL = "INSERT INTO auction_user (user_username, user_givenname, " +
         "user_familyname, user_email, user_password) VALUES (?, ?, ?, ?, ?)";
 
-    db.get_pool().getConnection()
-        .then(function () {
-            return db.get_pool().query(createSQL, values);
-        })
+    db.get_pool().query(createSQL, values)
         .then(function (result) {
             let json = {
                 "id": result.insertId
@@ -23,10 +20,7 @@ exports.create = function(values, done) {
 exports.view = function (id, done) {
     const viewSQL = "SELECT * FROM view_auction_user WHERE userId = ?";
 
-    db.get_pool().getConnection()
-        .then(function () {
-            return db.get_pool().query(viewSQL, id);
-        })
+    db.get_pool().query(viewSQL, id)
         .then(function (rows) {
             if (rows.length === 0) {
                 return done({"status": 404, "statusMessage": "Not found"});
@@ -69,15 +63,14 @@ function buildUpdateUserSQL(id, params) {
 }
 
 exports.change = function (id, changes, done) {
-    db.get_pool().getConnection()
-        .then(function () {
-            // TODO: Should an error be returned if an invaid property is given in the request?
-            // TODO: Should an error be returned if no valid properties are given?
-            let updateSQL = buildUpdateUserSQL(id, changes);
-            if (updateSQL !== "") {
-                return db.get_pool().query(updateSQL);
-            }
-        })
+    // TODO: Should an error be returned if an invaid property is given in the request?
+    // TODO: Should an error be returned if no valid properties are given?
+    let updateSQL = buildUpdateUserSQL(id, changes);
+    if (updateSQL === "") {
+		return done({"status": 400, "statusMessage": "Malformed request."});
+    }
+
+    db.get_pool().query(updateSQL)
         .then(function (result) {
             // TODO: Should an error be returned if the given id does not exist?
             return done({"status": 201, "statusMessage": "OK"});
