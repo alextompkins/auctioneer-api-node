d5d4a16517118f5d1d0ca48986a993e3417aae82
Alex Tompkins <ato47@uclive.ac.nz>
2018-05-15 21:54:38 +1200

    Changed express.js to allow cross origin requests.
    Took 7 minutes

--- config/express.js
+++ config/express.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const allowCrossOriginRequests = function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+};
+
 module.exports = function() {
     // INITIALISE EXPRESS //
     const app = express();
     app.use(bodyParser.json());
+    app.use(allowCrossOriginRequests);
     app.rootUrl = "/api/v1";
 
     // ROUTES //
